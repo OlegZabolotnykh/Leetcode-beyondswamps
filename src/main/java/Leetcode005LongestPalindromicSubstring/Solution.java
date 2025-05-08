3fc27c3e57cfe17aa132b397dbcaa33d62d58021
OlegZabolotnykh <oleg.zabolotnykh@gmail.com>
2025-05-08 12:14:14 +0300

    Leetcode 005: Changed int[][] to Boolean[][] (for checking whether time is improved). Accepted but 500ms > 16ms (best other result)

--- src/main/java/Leetcode005LongestPalindromicSubstring/Solution.java
+++ src/main/java/Leetcode005LongestPalindromicSubstring/Solution.java
@@ -12,15 +12,15 @@ class Solution {
 
     public String longestPalindrome(String s) {
         int n = s.length();
-        int[][] memotab = new int[n + 1][n + 1];
+        Boolean[][] memotab = new Boolean[n + 1][n + 1];
         int maxLen = 1;
         int resLeft = 0;
-        int resRight = 1;
+        int resRight = 0;
 
         for (int window = 1; window <= n; window++) {
             for (int l = 0, r; l + window <= n; l++) {
                 r = l + window - 1;
-                if (checkPalindrome(l, r, s, memotab) == 1) {
+                if (checkPalindrome(l, r, s, memotab)) {
                     if (window > maxLen) {
                         maxLen = window;
                         resLeft = l;
@@ -32,21 +32,21 @@ class Solution {
         return s.substring(resLeft, resRight + 1);
     }
 
-    int checkPalindrome(int l, int r, String s, int[][] memotab) {
+    boolean checkPalindrome(int l, int r, String s, Boolean[][] memotab) {
         if (r == l) {
-            return memotab[l][r] = 1;
+            return memotab[l][r] = true;
         }
 
-        if (memotab[l][r] != 0) {
+        if (memotab[l][r] != null) {
             return memotab[l][r];
         }
         if (Character.toLowerCase(s.charAt(l)) == Character.toLowerCase(s.charAt(r))) {
             if (r - l == 1) {
-                return memotab[l][r] = 1;
+                return memotab[l][r] = true;
             }
             return memotab[l + 1][r - 1] = checkPalindrome(l + 1, r - 1, s, memotab);
         } else {
-            return memotab[l][r] = -1;
+            return memotab[l][r] = false;
         }
     }
 }
