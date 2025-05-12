7b6762c2876a9e37e741a2c38f4bf84df48a5356
OlegZabolotnykh <oleg.zabolotnykh@gmail.com>
2025-05-12 20:53:07 +0300

    Leetcode 006: Accepted 15ms > 1ms (best)

--- src/main/java/Leetcode006ZigZagConversion/Solution.java
+++ src/main/java/Leetcode006ZigZagConversion/Solution.java
@@ -2,11 +2,12 @@ package main.java.Leetcode006ZigZagConversion;
 
 public class Solution {
     public static void main(String[] args) {
-        String result = new Solution().convert("PAYPALISHIRING", 4);
+        String result = new Solution().convert("AB", 1);
 
     }
 
     public String convert(String s, int numRows) {
+        if (s.length() == 1 || numRows == 1) return s;
         int constant1 = 2 * numRows - 2;
         int constant2 = numRows - 1;
         int n = s.length();
@@ -14,15 +15,10 @@ public class Solution {
                 ? (n - (n / constant1 + 1) * constant2)
                 : (n / constant1 * constant2 + 1);
         char[][] table = new char[numRows][numCols];
-//        for (int m = 0; m < numCols; m++) {
-//            for (int k = 0; k < numRows; k++) {
-//                table[k][m] = ' ';
-//            }
-//        }
 
         for (int i = 0; i < n; i++) {
-            if (i % constant1 > 3) {
-                table[constant1 - i % constant1][i - (i / constant1 + 1) * 3] = s.charAt(i);
+            if (i % constant1 > constant2) {
+                table[constant1 - i % constant1][i - (i / constant1 + 1) * constant2] = s.charAt(i);
             } else {
                 table[i % constant1][i / constant1 * constant2] = s.charAt(i);
             }
@@ -30,14 +26,11 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                System.out.print(table[row][col]);
                 if (table[row][col] != 0) {
                     sb.append(table[row][col]);
                 }
             }
-            System.out.println();
         }
-        System.out.println(sb.toString());
         return sb.toString();
     }
 }
